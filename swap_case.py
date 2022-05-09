aee882258b366cee17c84e8c2c66e70091e1eae5
Scott Wedge <scott.wedge@gmail.com>
2022-05-08 21:52:38 -0400

    Initial commit for 'swap case' challenge

---
+++ swap_case.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Challenge:
+# You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
+# 
+# For Example:
+# Www.HackerRank.com → wWW.hACKERrANK.COM
+# Pythonist 2 → pYTHONIST 2  
+# 
+# Function Description: Complete the swap_case function in the editor below.
+# 
+# swap_case has the following parameters:
+# string s: the string to modify
+# Returns: string: the modified string
+# Input Format: A single line containing a string
+# 
+# Sample Input: HackerRank.com presents "Pythonist 2".
+# Sample Output: hACKERrANK.COM PRESENTS "pYTHONIST 2".
+
+
+
